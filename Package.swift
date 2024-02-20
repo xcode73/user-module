69a86c8a0fdee79d4858c0fcb463eb1d5bc9507a
xcode73 <xcode.uln@gmail.com>
2024-02-20 16:18:12 +0400

    change to macos10

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "user-module",
     platforms: [
-       .macOS(.v12)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "UserModule", targets: ["UserModule"]),
