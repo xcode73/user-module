d0c8bdf564adcb114fdf7d5de2a722d77a7b3738
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:44:28 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,26 +1,27 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "user-module",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "UserModule", targets: ["UserModule"]),
+        .library(name: "UserModule", targets: ["UserModule"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/user-objects", .branch("test-dev")),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/user-objects", branch: "test-dev")
     ],
     targets: [
         .target(name: "UserModule",
                 dependencies: [
                     .product(name: "UserObjects", package: "user-objects"),
-                    .product(name: "Feather", package: "feather-core"),
+                    .product(name: "Feather", package: "feather-core")
                 ],
                 resources: [
-//                    .copy("Bundle"),
+//                    .copy("Bundle")
                 ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
